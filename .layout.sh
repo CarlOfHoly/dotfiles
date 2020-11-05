4e46f0b0b1758a460dca3bcbb73599d7cc39e27d
carlofholy <carl.smestad@gmail.com>
2020-11-05 20:01:55 +0100

    add nvidia support

--- .layout.sh
+++ .layout.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-xrandr --output DVI-D-1 --off --output DP-1 --primary --mode 3440x1440 --pos 0x240 --rotate normal --scale 1.5x1.5 --output DP-2 --off --output HDMI-1 --mode 1920x1080 --pos 5160x0 --rotate left --scale 1.5x1.5 --output DP-3 --off
+xrandr --output DVI-D-1 --off --output DP-4 --primary --mode 3440x1440 --pos 0x240 --rotate normal --scale 1.5x1.5 --output DP-2 --off --output HDMI-0 --mode 1920x1080 --pos 5160x0 --rotate left --scale 1.5x1.5 --output DP-3 --off
